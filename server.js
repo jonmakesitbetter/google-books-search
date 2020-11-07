7e5bfe7b9e02c30041cb791dd1b23494e609252e
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-07 16:05:53 -0500

    npm run build push

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static("client/build"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -33,6 +34,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
