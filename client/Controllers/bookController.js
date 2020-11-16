e8bd5481640c888492d4b512027d31f9846149e5
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-15 21:58:54 -0500

    Built controller with get, post, and delete routes

--- client/Controllers/bookController.js
+++ client/Controllers/bookController.js
@@ -1,11 +1,37 @@
-const router = require("router");
+const router = express.router;
 const express = require("express");
 const db = require("../src/components/Models/Books");
 
-router.get()
+router.get("/api/books", (req, res)=>{
+    db.Books.find({}).then((allBooks)=>{
+        res.json(allBooks)
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
 
-router.post()
+router.post("/api/books", (req, res)=>{
+    db.Books.create({
+        title: req.body.title,
+        authors: req.body.authors,
+        description: req.body.description,
+        image: req.body.image,
+        link: req.body.link
+    }).then((saveBook)=>{
+        res.json(saveBook);
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
 
-router.delete()
+router.delete("/api/books/:id", (req, res)=>{   
+    db.Books.delete(req.params.id)
+    .then((deleteBook)=>{
+        console.log(deleteBook);
+        res.json(deleteBook);
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
 
 module.exports = router;
