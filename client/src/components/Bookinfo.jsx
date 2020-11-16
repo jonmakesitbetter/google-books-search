7aa3c448a524b262a8999aa3d2d355e69c19a804
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-15 21:38:22 -0500

    Finished Bookinfo return portion

---
+++ client/src/components/Bookinfo.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Axios from "axios";
+
+const Bookinfo = (props) => {
+
+    const showBook = ()=>{
+        let path = props.ink;
+        window.open(path);
+    };
+
+    const deleteBook = (e)=>{
+        const id = e.target.getAttribute(id);
+
+        Axios.delete(`/api/books/${id}`).then((result)=>{
+            props.loadBooks();
+        })
+    }
+    return (
+        <div>
+           <li id={props.id}>
+               <h2>{props.title}</h2>
+               <img src={props.thumbnail} alt={props.title}/>
+               <h2>Author: {props.authors}</h2>
+               <p>{props.description}</p>
+               <button className="btn btn-primary" onClick={showBook}>Book!</button>
+               {props.onSearch ? (
+                   <button className="btn btn-primary" onClick={()=>props.saveBook(props.id)}>Save</button>
+               ) : (
+                   <button className="btn btn-danger" onClick={deleteBook}>Delete</button>
+               )
+               }
+           </li>
+        </div>
+    );
+};
+
+export default Bookinfo;
